fb33967247b2183c2eeb293aa3f67157050bdb88
PL45M4 <60303087+PL45M4@users.noreply.github.com>
2020-01-27 17:13:18 +0700

    Add files via upload

---
+++ Tools.sh
@@ -0,0 +1,142 @@
+#!system/xbin/bash
+clear
+login="TERMUXID123"
+while [ "$siapa" != "$login" ]
+do
+     read -p "Silahkan Login :" siapa
+     done
+                        clear
+echo "\033[31;1m   _______ ______ _____  __  __ _    ___   _______ _____ "
+echo "\033[31;1m  |__   __|  ____|  __ \|  \/  | |  | \ \ / /_   _|  __ \ "
+echo "\033[31;1m     | |  | |__  | |__) | \  / | |  | |\ V /  | | | |  | |"
+echo "\033[37;1m     | |  |  __| |  _  /| |\/| | |  | | > <   | | | |  | |"
+echo "\033[37;1m     | |  | |____| | \ \| |  | | |__| |/ . \ _| |_| |__| |"
+echo "\033[37;1m     |_|  |______|_|  \_\_|  |_|\____//_/ \_\_____|_____/ "
+        echo " "
+echo "\033[32;1m__________________________________________"
+echo "\033[36;1m>>>A U T H O R       : .XPL45M4"
+echo "\033[36;1m>>>C h a n e l       : ESPH CHANEL"
+echo "\033[36;1m>>>F A C E B O O K   : ErlanggaBLP"
+echo "\033[36;1m>>>C O N T A C T     : 0852********"
+echo "\033[36;1m>>>M Y T E A M       : BLACK CYBER TEAM"
+echo "\033[32;1m__________________________________________"
+echo " "
+echo "\033[31;1m======================================================================"
+echo "\033[37;1m[ 1 ] DARK FB                                                   [VULN]"
+echo "\033[31;1m======================================================================"
+echo "\033[37;1m[ 2 ] HACK INSTAGRAM                                            [VULN]"
+echo "\033[31;1m======================================================================"
+echo "\033[37;1m[ 3 ] SPAM TELPON                                               [VULN]"
+echo "\033[31;1m======================================================================"
+echo "\033[37;1m[ 4 ] SPAM SMS                                                  [VULN]"
+echo "\033[31;1m======================================================================"
+echo "\033[37;1m[ 5 ] EXIT                                                    [KELUAR]"
+echo "\033[31;1m======================================================================"
+echo " "
+read -p "Silahkan Pilih Tools:" pilih
+if  [ $pilih = "1" ]
+then
+$Home
+clear
+echo "\033[32;1mMasukkan ID :"
+read PERSON
+sleep 2
+clear
+echo "\033[31;1mSedang Crack Password : $PERSON"
+echo " "
+echo "\033[37;1m====================================="
+sleep 5
+echo "\033[31;1mMencoba ====> lupasandi      [SALAH]"
+sleep 4
+echo "\033[31;1mMencoba ====> jangandibajak  [SALAH]"
+sleep 3
+echo "\033[31;1mMencoba ====> anjing1234     [SALAH]"
+sleep 2
+echo "\033[31;1mMencoba ====> 1234567890     [SALAH]"
+sleep 1
+echo "\033[31;1mMencoba ====> lupasandi123   [SALAH]"
+sleep 1
+echo "\033[31;1mMencoba ====> 09876543217    [SALAH]"
+echo "\033[37;1m====================================="
+clear
+echo "Password Ditemukan!!!"
+echo " "
+echo "\033[32;1m___________________________"
+echo "\033[32;1m|"                      "                         \033[32;1m|"
+echo "\033[32;1m|"                      "                         \033[32;1m|"
+echo "\033[32;1m|" "\033[36;1mPassword : lupa12345567"" \033[32;1m|"
+echo "\033[32;1m|"                      "                         \033[32;1m|"
+echo "\033[32;1m|"                      "                         \033[32;1m|"
+echo "\033[32;1m___________________________"
+fi
+
+if [ $pilih = "2" ]
+then
+ $home
+clear
+echo "\033[32;1mMasukkan Username:"
+read PERSON
+sleep 2
+clear
+echo "Sedang mencari Password, Harap tunggu sebentar..."
+sleep 3
+clear
+echo "Password sudah di temukan!!!"
+echo " "
+echo "USERNAME : $PERSON"
+echo "PASSWORD : akusayangkamu"
+echo " "
+fi
+
+if [ $pilih = "3" ]
+then
+ $home
+ clear
+ apt update && apt upgrade
+apt install php
+apt install git
+apt install toilet
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pilih = "4" ]
+then
+ $home
+ clear
+ apt update && apt upgrade
+pkg install git
+pkg install python2
+pkg install php curl
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutalsms
+python2 run.py
+fi
+
+if [ $pilih = "5" ]
+then
+ $home
+clear
+echo "TERIMA KASIH TELAH MENGGUNAKAN TOOLS INI... "
+sleep 1
+echo " "
+echo "JANGAN LUPA BERIBADAH"
+sleep 1
+echo ""
+echo "see you..."
+sleep 1
+clear
+exit
+fi
+
+clear
+sleep 1
+echo "Maaf Pilihan Yang Anda Tidak Tersedia!!!"
+echo " "
+echo "\033[37;1mSilahkan Login Ulang"
+echo " "
+
+sleep 2
+clear
+sh TERMUX.sh
